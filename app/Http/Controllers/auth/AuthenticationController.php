5c175f1bcce5dfd3c75382f5cc3228e966372e63
Jack Weinbender <jack.weinbender@gmail.com>
2015-09-27 15:00:42 -0500

    Basic Auth functions (Needs JWT still); User seeding

--- app/Http/Controllers/auth/AuthenticationController.php
+++ app/Http/Controllers/auth/AuthenticationController.php
@@ -5,15 +5,34 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \Input;
+use Hash;
+use App\User;
 
 class AuthenticationController extends Controller
 {
     public function loginWithCredentials(Request $request)
     {
-      if(!Input::has('data')){
-        return response('No payload provided', 400);
+      if(Input::has('email') && Input::has('password')){
+
+        $user = User::where('email', Input::get('email'))->first();
+
+        if($user){
+
+          if(Hash::check(Input::get('password'), $user->password)){
+
+            return response('Logged In Response', 200);
+
+          }
+          
+          return response('Incorrect Email or Password', 400);
+
+
+        }
+
+        return response('Incorrect Email or Password', 400);
+
       }
 
-      return response('Logged In Response', 200);
+      return response('You must include both an email address and password', 400);
     }
 }
--- tests/auth/AuthenticationControllerTest.php
+++ tests/auth/AuthenticationControllerTest.php
@@ -1,36 +1,75 @@
 <?php
 
-use App\Letter;
+use App\User;
 
 class AuthenticationControllerTest extends ApiTestCase
 {
 
-  public function testLoginWithCredentialsWithoutPayload()
+  public function testLoginWithoutPayload()
   {
-    $this->post('auth/login')
-      ->see('No Payload Provided')
+    $this->post('login')
+      ->see('You must include both an email address and password')
+      ->assertResponseStatus(400);
+  }
+  public function testLoginWithoutEmail()
+  {
+    $payload['password'] = 'password';
+
+    $this->post('login', $payload)
+      ->see('You must include both an email address and password')
+      ->assertResponseStatus(400);
+  }
+  public function testLoginWithoutPassword()
+  {
+    $payload['email'] = 'email@email.com';
+
+    $this->post('login', $payload)
+      ->see('You must include both an email address and password')
+      ->assertResponseStatus(400);
+  }
+  public function testLoginWithBadEmail()
+  {
+    $user = factory(User::class)->make();
+
+    $payload = array(
+      'email' => 'wrong@wrong.com',
+      'password' => $user->password,
+    );
+
+    $user->save();
+
+    $this->post('login', $payload)
+      ->see('Incorrect Email or Password')
+      ->assertResponseStatus(400);
+  }
+  public function testLoginWithBadPassword()
+  {
+    $user = factory(User::class)->make();
+
+    $payload = array(
+      'email' => $user->email,
+      'password' => 'badPassword',
+    );
+
+    $user->save();
+
+    $this->post('login', $payload)
+      ->see('Incorrect Email or Password')
       ->assertResponseStatus(400);
   }
   public function testLoginWithCredentialsWithPayload()
   {
-    $this->post('auth/login', $this->getUserPayload())
+    $user = factory(User::class)->make();
+
+    $payload = array(
+      'email' => $user->email,
+      'password' => 'password',
+    );
+
+    $user->save();
+
+    $this->post('login', $payload)
       ->see('Logged In Response')
       ->assertResponseOK();
-  }
-
-  /**
-   * Pirate functions
-   */
-  private function getUserAttributes(){
-    return array(
-      'email' => $this->fake->email,
-      'password' => $this->fake->password,
-    );
-  }
-
-  private function getUserPayload(){
-    return array(
-      "data" => $this->getUserAttributes(),
-    );
   }
 }
